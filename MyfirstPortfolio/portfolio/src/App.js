76476e85acff6a7880761e3a37ae36ddd8ac37d5
MariKvan <Marikvanch@gmail.com>
2020-11-11 10:06:28 +0100

    React portfolio

---
+++ MyfirstPortfolio/portfolio/src/App.js
@@ -0,0 +1,41 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        < nav>
+          <ul>
+            <li><a href="#">Intro</a></li>
+            <li><a href="#">Who</a></li>
+            <li><a href="#">Work</a></li>
+            <li><a href="#">Find Me</a></li>
+          </ul>
+        </nav>
+      </header>
+
+      <main>
+        <div className="container"></div>
+
+        <div className="flex-container">
+
+          <div>Graphic</div>
+          <div>Designer</div>
+          <div>Illustrator</div>
+          <div>Creative</div>
+          <div>Front</div>
+          <div>-</div>
+          <div>End</div>
+          <div>Developer</div>
+          <div>@Your service</div>
+
+          </div>
+      
+
+      </main>
+    </div>
+  );
+}
+
+export default App;
